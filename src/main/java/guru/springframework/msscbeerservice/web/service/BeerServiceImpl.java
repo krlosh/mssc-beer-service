bbdd2a5f6ff7821f7baf16583316ecab5bed640c
carloshenche <carloshenche@metricool.com>
2019-12-13 09:43:46 +0100

    Adding list beers endpoint

--- src/main/java/guru/springframework/msscbeerservice/web/service/BeerServiceImpl.java
+++ src/main/java/guru/springframework/msscbeerservice/web/service/BeerServiceImpl.java
@@ -5,10 +5,16 @@ import guru.springframework.msscbeerservice.repositories.BeerRepository;
 import guru.springframework.msscbeerservice.web.controller.NotFoundException;
 import guru.springframework.msscbeerservice.web.mappers.BeerMapper;
 import guru.springframework.msscbeerservice.web.model.BeerDto;
+import guru.springframework.msscbeerservice.web.model.BeerPageList;
+import guru.springframework.msscbeerservice.web.model.BeerStyleEnum;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -41,5 +47,31 @@ public class BeerServiceImpl implements BeerService {
         beer.setUpc(beerDto.getUpc());
 
         return this.mapper.beerToBeerDto(beer);
+    }
+
+    @Override
+    public BeerPageList listBeers(String beerName, BeerStyleEnum beerStyle, PageRequest pageRequest) {
+        Page<Beer> page;
+        if (!StringUtils.isEmpty(beerName) && !StringUtils.isEmpty(beerStyle)) {
+            page = this.repository.findAllByBeerNameAndBeerStyle(beerName, beerStyle, pageRequest);
+        }
+        else if (!StringUtils.isEmpty(beerName) && StringUtils.isEmpty(beerStyle)) {
+            page = this.repository.findAllByBeerName(beerName, pageRequest);
+        }
+        else if(StringUtils.isEmpty(beerName) && !StringUtils.isEmpty(beerStyle)) {
+            page = this.repository.findAllByBeerStyle(beerStyle, pageRequest);
+        }
+        else {
+            page = this.repository.findAll(pageRequest);
+        }
+        return new BeerPageList(page
+                            .getContent()
+                            .stream()
+                            .map(this.mapper::beerToBeerDto)
+                            .collect(Collectors.toList()),
+                            PageRequest.of(page.getPageable().getPageNumber(),
+                                    page.getPageable().getPageSize()),
+                            page.getTotalElements()
+                );
     }
 }
